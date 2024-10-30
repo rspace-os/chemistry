3342fb18f2369e66d06bbe2a3b9a9c3908a28f0e
fraser.macinnes <fraser.macinnes@researchspace.com>
2024-10-30 14:19:53 +0000

    init

---
+++ src/main/java/com/github/rspaceos/chemistry/ChemistryApplication.java
@@ -0,0 +1,13 @@
+package com.github.rspaceos.chemistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChemistryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChemistryApplication.class, args);
+  }
+
+}
