c9b092824f97989eaceaadbf6faba4e60062c8c1
rs-fraser <fraser.macinnes>
2025-03-06 14:35:43 +0000

    abbreviate log message

--- src/main/java/com/researchspace/chemistry/util/CommandExecutor.java
+++ src/main/java/com/researchspace/chemistry/util/CommandExecutor.java
@@ -13,6 +13,8 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Consumer;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -35,7 +37,7 @@ public class CommandExecutor {
     Future<?> future = executorService.submit(streamConsumer);
     process.waitFor();
     future.get(30, TimeUnit.SECONDS);
-    LOGGER.info("Found output: {}", String.join(", ", output));
+    LOGGER.info("Found output: {}", StringUtils.abbreviate(String.join(", ", output), 500));
     return output;
   }
 
