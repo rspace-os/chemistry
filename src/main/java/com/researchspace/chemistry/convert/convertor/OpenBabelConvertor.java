a29db136900635053158e50cb09295d470df263d
Fraser <141634676+rs-fraser@users.noreply.github.com>
2025-03-07 15:33:51 +0000

    PRT-896 meta information not showing (#14)
    * use regex to match openbabel error converting (which prints the openbabel version) to 3.x.x
    
    * add startup check for openbabel version. if openbabel version isn't 3.x.x log and fail to start to ensure compatibility.
    
    ---------
    
    Co-authored-by: rs-fraser <fraser.macinnes>

--- src/main/java/com/researchspace/chemistry/convert/convertor/OpenBabelConvertor.java
+++ src/main/java/com/researchspace/chemistry/convert/convertor/OpenBabelConvertor.java
@@ -3,23 +3,46 @@ package com.researchspace.chemistry.convert.convertor;
 import com.researchspace.chemistry.ChemistryException;
 import com.researchspace.chemistry.convert.ConvertDTO;
 import com.researchspace.chemistry.util.CommandExecutor;
+import jakarta.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OpenBabelConvertor implements Convertor {
-  public static final String OPENBABEL_ERROR_OUTPUT_IDENTIFIER = "Open Babel 3.1.0 -- ";
+  private static final Logger LOGGER = LoggerFactory.getLogger(OpenBabelConvertor.class);
+  private static final String OPENBABEL_EMPTY_OUTPUT_3XX = "Open Babel 3.\\d+.\\d+ --";
+  private final Pattern pattern = Pattern.compile(OPENBABEL_EMPTY_OUTPUT_3XX);
 
   private final CommandExecutor commandExecutor;
 
   public OpenBabelConvertor(CommandExecutor commandExecutor) {
     this.commandExecutor = commandExecutor;
+  }
+
+  @PostConstruct
+  public void init() {
+    try {
+      LOGGER.info("Checking OpenBabel version compatibility.");
+      ProcessBuilder builder = new ProcessBuilder();
+      builder.command("obabel");
+      String output = String.join("\n", commandExecutor.executeCommand(builder));
+      Matcher matcher = pattern.matcher(output);
+      if (!matcher.find()) {
+        throw new ChemistryException("OpenBabel version isn't 3.X.X so may be incompatible.");
+      }
+    } catch (IOException | ExecutionException | InterruptedException | TimeoutException e) {
+      throw new ChemistryException("Problem while initializing OpenBabel.", e);
+    }
   }
 
   @Override
@@ -36,13 +59,18 @@ public class OpenBabelConvertor implements Convertor {
           "-o" + convertDTO.outputFormat());
       String output = String.join("\n", commandExecutor.executeCommand(builder));
       Files.delete(tempFile.toPath());
-      if (output.isEmpty() || output.startsWith(OPENBABEL_ERROR_OUTPUT_IDENTIFIER)) {
+      if (output.isEmpty() || isFailedConversionOutput(output)) {
         return Optional.empty();
       }
       return Optional.of(output);
     } catch (IOException | ExecutionException | InterruptedException | TimeoutException e) {
       throw new ChemistryException("Problem while converting.", e);
     }
+  }
+
+  private boolean isFailedConversionOutput(String output) {
+    Matcher matcher = pattern.matcher(output);
+    return matcher.find();
   }
 
   private byte[] getChemBytes(String input) {
